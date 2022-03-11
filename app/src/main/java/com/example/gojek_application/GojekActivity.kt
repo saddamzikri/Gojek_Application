4a05b0d8c7d735fb4d9280f5454d75148c3f8010
saddamzikri <msaddamzd@gmail.com>
2022-03-11 23:48:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gojek_application/GojekActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gojek_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GojekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
